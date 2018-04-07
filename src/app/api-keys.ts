ef9c281a2dd00541d3269468550010373f8e3662
russell hofvendahl <rhofvendahl@gmail.com>
2018-04-06 21:51:52 -0700

    track firebase credentials for heroku

---
+++ src/app/api-keys.ts
@@ -0,0 +1,8 @@
+export var masterFirebaseConfig = {
+    apiKey: "AIzaSyAU_4NTQuIMgV5axarVGRZay_9K4zDugws",
+    authDomain: "simcellular-9b51f.firebaseapp.com",
+    databaseURL: "https://simcellular-9b51f.firebaseio.com",
+    projectId: "simcellular-9b51f",
+    storageBucket: "simcellular-9b51f.appspot.com",
+    messagingSenderId: "463682200877"
+  };
